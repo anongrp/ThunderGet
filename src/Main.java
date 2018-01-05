d4214bcd086df6af65c2337c1f09af8d0bcaa509
Anikesh Patel <anikeshpatel4@gmail.com>
2018-01-05 13:50:17 +0530

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws MalformedURLException {
+        URL website = new URL("https://github.com/Anikeshpatel/AnDatabase/blob/master/images/AnonDatabase.png");
+        try (InputStream in = website.openStream()) {
+            Files.copy(in, Paths.get(""), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
